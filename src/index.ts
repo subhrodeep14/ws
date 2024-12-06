fac6299b06e986c138e3df31a40c5a8bceddf387
Subhrodeep Acharya <subhrodeepacharya14@gmail.com>
2024-12-07 01:01:12 +0530

    ping pong be done

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+
+import {WebSocketServer} from "ws"
+ const wss =new WebSocketServer({port:8080});
+
+ wss.on("connection",function(socket){
+        socket.on("message",(e)=>{
+            if(e.toString()==="ping"){
+                socket.send("pong")
+            }
+        })
+    
+ })
